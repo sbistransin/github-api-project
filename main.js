529b4b3196d6a900a3bb7cae768dd770e12595f4
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-02-02 14:36:08 -0500

    render results to page

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//var users = [];
+
 var onSubmitClick = function() {
   var input = $('#hash-input').val();
   fetch(input);
@@ -6,7 +8,7 @@ var onSubmitClick = function() {
 var fetch = function(query) {
   $.ajax({
     method: "GET",
-    url: "https://api.github.com/repos/facebook/react/commits/e09097a75da040f428ca335e9d181186a61247d1",
+    url: "https://api.github.com/repos/facebook/react/commits/" + query,
     dataType: "json",
     success: function(data) {
       addUser(data);
@@ -21,7 +23,17 @@ var addUser = function(data) {
   var userData = {
     username: data.author.login,
     image: data.author.avatar_url 
-  }
+  };
+  //users.push(userData);
+  renderHTML(userData);
+}
+
+var renderHTML = function(user) {
+  var source = $('#github-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(user);
+  $('.faces').append(newHTML);
+
 }
 $('#hash-submit').on("click", onSubmitClick);
 

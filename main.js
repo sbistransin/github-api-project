cc9e1b803a7f2dccafed23f1dfba5bc9f02d1628
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-02-02 11:15:43 -0500

    get login and user picture

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 var onSubmitClick = function() {
+  var input = $('#hash-input').val();
+  fetch(input);
 };
 
+var fetch = function(query) {
+  $.ajax({
+    method: "GET",
+    url: "https://api.github.com/repos/facebook/react/commits/e09097a75da040f428ca335e9d181186a61247d1",
+    dataType: "json",
+    success: function(data) {
+      addUser(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+}
+
+var addUser = function(data) {
+  var userData = {
+    username: data.author.login,
+    image: data.author.avatar_url 
+  }
+}
 $('#hash-submit').on("click", onSubmitClick);
 
 
